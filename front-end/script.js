4094bc697de7f8b0bdbfd540e4f5c75c318816a2
Tiago Farias <tiago.hfarias@outlook.com>
2026-09-05 09:25:43 -0300

    Adicionei o svg do mapa do brasil e o botão Enter para enviar

--- front-end/script.js
+++ front-end/script.js
@@ -1,10 +1,37 @@
 // 1. MAPEAMENTO DE ESTADOS
 // Objeto de estados válidos --- chave: nome do estado, Valor: id do elemento SVG correspondente 
 const mapaEstados = {
-    'sao paulo': 'sp',
-    'rio de janeiro': 'rj',
-    // Adicione outros estados conforme necessário
-}
+    "acre": "ac",
+    "alagoas": "al",
+    "amapa": "ap",
+    "amazonas": "am",
+    "bahia": "ba",
+    "ceara": "ce",
+    "distrito federal": "df",
+    "espirito santo": "es",
+    "goias": "go",
+    "maranhao": "ma",
+    "mato grosso": "mt",
+    "mato grosso do sul": "ms",
+    "minas gerais": "mg",
+    "para": "pa",
+    "paraiba": "pb",
+    "parana": "pr",
+    "pernambuco": "pe",
+    "piaui": "pi",
+    "rio de janeiro": "rj",
+    "rio grande do norte": "rn",
+    "rio grande do sul": "rs",
+    "rondonia": "ro",
+    "roraima": "rr",
+    "santa catarina": "sc",
+    "sao paulo": "sp",
+    "sergipe": "se",
+    "tocantins": "to"
+};
+
+const totalEstados = Object.keys(mapaEstados).length;
+
 
 const estadosDescobertos = [];
 let acertos = 0;
@@ -12,7 +39,7 @@ let acertos = 0;
 //2.ELEMENTOS DOM
 // Selecionando os elementos DOM
 const entradaEstado = document.getElementById('entrada-estado');
-const btn = document.getElementById('btn-teste');
+const btn = document.getElementById('button');
 const mensagem = document.getElementById('mensagem');
 
 //3. FUNÇÃO AUXILIAR 
@@ -40,14 +67,26 @@ btn.addEventListener('click', function() {
         //pinta o estado no mapa
         const elementoSvg = document.getElementById(idEstado);
         elementoSvg.classList.add('descoberto');
-        
+
         //adiciona o estado à lista de descobertos
         estadosDescobertos.push(idEstado);
         acertos++; 
 
-        mensagem.textContent = 'Parabéns! Você acertou o estado!';
+        mensagem.textContent = `Parabéns! ${acertos} / ${Object.keys(mapaEstados).length} estados descobertos.`;
         entradaEstado.value = ''; // Limpa o campo de entrada
+    
+    } else if (estadosDescobertos.includes(idEstado)) {
+        mensagem.textContent = 'Você ja descobriu esse estado. Tente outro.';
+        entradaEstado.value = '';
+    
     } else {
         mensagem.textContent = 'Estado inválido. Tente novamente.';
     }
+});
+     
+    //Escuta a tecla pressionada dentro do campo
+    entradaEstado.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            btn.click(); //Simula o clique no botão com a tecla Enter
+        }
 });
